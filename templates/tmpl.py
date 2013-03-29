5bae6d9540816d1b11c9fdd4e537f8c29fee8179
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2013-03-29 07:15:23 -0400

    setting things up

---
+++ templates/tmpl.py
@@ -0,0 +1,25 @@
+# This program is free and subject to the conditions of the MIT license.
+# If you care to read that, here's a link:
+# http://opensource.org/licenses/MIT
+
+#===================== IMPORTS --- SETUP --- GLOBAL VARS ======================
+import os
+import sys
+import re
+
+#============================ Boolean Functions ===============================
+
+
+#============================ Helper Functions ================================
+
+
+#============================ Core Functions ==================================
+
+
+#================================= __MAIN__ ===================================
+def main():
+    pass
+
+#------------------------------------------------------------------------------
+#if __name__ == '__main__':
+    #main()
